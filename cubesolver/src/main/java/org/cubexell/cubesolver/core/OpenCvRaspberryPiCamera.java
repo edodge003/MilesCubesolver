583f03fef90864fc71e2f0c190388284dc17e75e
cubesolver <edodge003@gmail.com>
2025-08-01 10:29:13 -0700

    new files

--- cubesolver/src/main/java/org/cubexell/cubesolver/core/OpenCvRaspberryPiCamera.java
+++ cubesolver/src/main/java/org/cubexell/cubesolver/core/OpenCvRaspberryPiCamera.java
@@ -169,8 +169,8 @@ public class OpenCvRaspberryPiCamera implements CubeColorInspector{
 
     }
 
-    int offsetX = -100;
-    int offsetY = -260;
+    int offsetX = -200;
+    int offsetY = -190;
 
     public char[][] inspectBackFace(char center) {
         face = 0;
